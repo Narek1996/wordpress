1c0d72011255aa92dafb36ae0f218af455ba5b5d
narek1996 <1996128n>
2017-06-25 01:41:53 +0400

    esor

--- wp-content/themes/aag/js/aag-script.js
+++ wp-content/themes/aag/js/aag-script.js
@@ -1,13 +1,24 @@
 $(document).ready(function () {
-/*sub menu*/
-$( "#menu div ul li:has(ul)").addClass('dropdown').children('a').removeAttr().attr({
-    "class":"dropdown-toggle",
-    "data-toggle":"dropdown",
-    "role":"button",
-    "aria-haspopup":"true",
-    "aria-expanded":"false"
-})
-    $( "#menu div ul li:has(ul)").children('ul').addClass('dropdown-menu').removeClass('sub-menu')
+    /*sub menu*/
+    $("#menu div ul li:has(ul)").addClass('dropdown-submenu dropdown').children('a').removeAttr().attr({
+        "d":"dLabel" ,
+        "ole":"button" ,
+        "ata-toggle":"dropdown",
+        "lass":"btn btn-primary" ,
+        "ata-target":"#"
+    });
+    $("#menu div ul li:has(ul)").attr({
+        "id":"dLabel",
+        "role":"button",
+        "data-toggle":"dropdown",
+        "data-target":"#"
+    });
+    $("#menu div ul li:has(ul)").children('ul').attr({
+        "role":"menu",
+        "aria-labelledby":"dropdownMenu"
+    }).addClass('dropdown-menu multi-level').removeClass('sub-menu');
+    $("li").closest('.header_navbar').children().addClass('general_li');
+
 
 
 })
